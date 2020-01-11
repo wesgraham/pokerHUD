e65fbedffc05c5a56cdaff84c28bcf0d3a352c81
Wesley Graham <wgraham@berkeley.edu>
2020-01-11 11:05:54 -0800

    hand

---
+++ pkg/types/hand.go
@@ -0,0 +1,14 @@
+package types
+
+type Hand struct {
+	Uname string
+	HandID int
+	Balance int
+	Hand string
+	PotSize int
+	Action string
+	Amount int
+	Board []string
+	ThreeBet bool
+	FourPlusBet bool
+}
