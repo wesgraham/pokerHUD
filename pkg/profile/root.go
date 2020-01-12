6bc74f6a38e1b888378f9222a9ed5f0e6d643f96
Wesley Graham <wgraham@berkeley.edu>
2020-01-12 12:37:52 -0800

    db fixes

--- pkg/profile/root.go
+++ pkg/profile/root.go
@@ -18,7 +18,7 @@ type Stats struct {
 
 func Get(username string) (Profile, error) {
 	// TODO: Remove need to hardcode query
-	query := "http://localhost:3000/hands?username=" + username
+	query := "http://localhost:3000/hands?username=eq." + username
 	data, err := store.Get(query)
 	if err != nil {
 		return Profile{}, fmt.Errorf("error retrieving query data: %s", err)
